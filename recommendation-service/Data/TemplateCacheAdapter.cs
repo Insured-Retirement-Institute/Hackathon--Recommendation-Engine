19fe84abd23d21a947357ce51bce7b4ade5b034e
Chris Ellenwood <Chris.Ellenwood@american-equity.com>
2025-02-12 11:58:36 -0600

    Added template caching

---
+++ recommendation-service/Data/TemplateCacheAdapter.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Caching.Memory;
+
+namespace Data;
+
+public class TemplateCacheAdapter(ITemplatesRepo repo, IMemoryCache cache) : ITemplatesRepo
+{
+    public async Task<Template?> GetTemplate(string name)
+    {
+        var template = await cache.GetOrCreateAsync(name, async entry 
+            => await repo.GetTemplate(name));
+
+        return template;
+    }
+}
--- recommendation-service/Data/_Configuration/DataConfiguration.cs
+++ recommendation-service/Data/_Configuration/DataConfiguration.cs
@@ -1,10 +1,21 @@
-﻿namespace Data._Configuration;
+﻿using Microsoft.Extensions.Caching.Memory;
+
+namespace Data._Configuration;
 
 public static class DataConfiguration
 {
     public static IServiceCollection AddData(this IServiceCollection services, IConfiguration config)
     {
-        services.AddScoped<ITemplatesRepo, TemplatesRepo>();
+        services.AddMemoryCache();
+
+        services.AddScoped<TemplatesRepo>();
+        services.AddScoped<ITemplatesRepo>(provider =>
+        {
+            var repo = provider.GetRequiredService<TemplatesRepo>();
+            var cache = provider.GetRequiredService<IMemoryCache>();
+            var adapter = new TemplateCacheAdapter(repo, cache);
+            return adapter;
+        });
 
         return services;
     }
